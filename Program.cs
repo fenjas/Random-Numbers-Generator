8f94dba618b37f94ada1bc8aa5ee4010f416037d
Jason Fenech <jason.fenech@gig.com>
2024-08-21 15:12:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+
+namespace Random_Numbers_Generator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> allTickets = [];
+            int plays = 1;
+            int replaced = 0;
+            string tickets_file = "tickets.txt";
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Clear();
+
+            if (File.Exists(tickets_file)) File.Delete(tickets_file);
+
+            void WriteTo(string msg)
+            {
+                Console.WriteLine(msg);
+                using StreamWriter sw = File.AppendText(tickets_file);
+                {
+                    sw.WriteLine(msg);
+                }
+            }
+
+            WriteTo("SUPER 5 TICKETS");
+            WriteTo("---------------");
+            
+            if (args.Length != 0)
+            {
+                if (int.TryParse(args[0], out plays))
+                {
+                    if (plays <= 0) plays = 1;
+                }
+                else
+                {
+                    plays = 1;
+                }
+            }
+
+            // Create tickets. Discard identical ones and re-play.
+            for (int i = 0; i < plays; i++)
+            {
+                var numbers = Super5.Play(Guid.NewGuid().GetHashCode());
+                var currentTicket = string.Join('-', numbers.Select(n => n.ToString("D2")));
+
+                if (!allTickets.Contains(currentTicket))
+                {
+                    allTickets.Add(currentTicket);
+                }
+                else
+                {
+                    i--; // replay
+                    replaced++;
+                }
+            }
+
+            // Display / Save tickets
+            allTickets.Sort();
+            StringBuilder sb = new();
+            foreach (var ticket in allTickets)
+            {
+                sb.AppendLine(ticket);
+            }
+            WriteTo(sb.ToString().TrimEnd());
+            
+
+            double o5 = Math.Round(Super5.CalculateOdds(5), 2);
+            double o4 = Math.Round(Super5.CalculateOdds(4), 2);
+            double o3 = Math.Round(Super5.CalculateOdds(3), 2);
+            string p5 = Math.Round((1 / o5), 8).ToString("0.########");
+            string p4 = Math.Round((1 / o4), 8).ToString("0.########");
+            string p3 = Math.Round((1 / o3), 8).ToString("0.########");
+
+            WriteTo("---------------");
+            WriteTo("");
+            WriteTo($"Odds of guessing 5 numbers per ticket is {o5} or p={p5}");
+            WriteTo($"Odds of guessing 4 numbers per ticket is {o4} or p={p4}");
+            WriteTo($"Odds of guessing 3 numbers per ticket is {o3} or p={p3}");
+            WriteTo("");
+            WriteTo("😀 GOOD LUCK 😀");
+            WriteTo("");
+            
+            WriteTo($"DEBUG::Generated {plays} tickets");
+            WriteTo($"DEBUG::Replaced {replaced} identical tickets");
+        }
+    }
+}
---
+++ Super5.cs
@@ -0,0 +1,56 @@
+﻿namespace Random_Numbers_Generator
+{
+    internal class Super5
+    {
+        private static readonly int super5_min = 1;
+        private static readonly int super5_max = 45;
+        private static readonly int super5_max_drawn = 5;
+
+        public static List<int> Play(int seed)
+        {
+            var r = new Random(seed);
+            var nList = new List<int>();
+      
+            for (int i = 0; i < super5_max_drawn; i++)
+            {
+                int n = r.Next(super5_min, super5_max + 1);
+         
+                if (!nList.Contains(n))
+                {
+                    nList.Add(n);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            nList.Sort();
+
+            return nList;
+        }
+
+        public static double CalculateOdds(int numbers)
+        {
+            if (numbers == 0) numbers = 1;
+
+            int n = super5_max;
+            int r = super5_max_drawn;
+            int k = numbers;
+
+            double odds_t = Factorial(n) / (Factorial(r) * Factorial(n - r));
+            double odds_i = Factorial(r) / (Factorial(k) * Factorial(r - k));
+            double odds_j = Factorial(n - r) / (Factorial((n - r) - (r - k)) * Factorial(r - k));
+
+            return odds_t / (odds_i * odds_j);
+        }
+
+        private static double Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
